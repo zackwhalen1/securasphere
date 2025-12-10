7c7defd9e7f97d89b9a19188e5d6b21a55485327
zackwhalen1 <zackwhalen1@gmail.com>
2025-12-09 21:21:27 -0600

    Removed old pages
    Removed old UI pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, useNavigate, useLocation } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import './App.css';
 import AboutPage from './AboutPage';
 import ContactPage from './ContactPage';
-import PhishingTrainer from './PhishingTrainer';
 import PhishingTrainerTool from './PhishingTrainerTool';
 import SocialMediaAnalyzerTool from './SocialMediaAnalyzerTool';
 import PhishingTrainingPage from './PhishingTrainingPage';
@@ -11,12 +10,7 @@ import PasswordSecurityPage from './PasswordSecurityPage';
 import SocialMediaAnalysisPage from './SocialMediaAnalysisPage';
 import UnifiedSocialMediaAnalyzer from './UnifiedSocialMediaAnalyzer';
 import PasswordGeneratorAndCheckerTool from './PasswordGeneratorAndCheckerTool';
-import PasswordChecker from './passwordchecker';
-import PasswordGenerator from './passwordgenerator';
-import PasswordInfo from './PasswordInfo';
-import PhishingInfo from './PhishingInfo';
 import PrivacyPage from './PrivacyPage';
-import SocialMediaInfo from './SocialMediaInfo';
 import TermsPage from './TermsPage';
 import HUDTestFixed from './HUDTestFixed';
 
@@ -38,26 +32,15 @@ function App() {
       <ScrollToTop />
       <Routes>
         {/*New Home*/}
-        <Route path="/test" element={<HUDTestFixed />} />
-
-        {/*Old Home */}
         <Route path="/" element={<HUDTestFixed />} />
         
-        {/*Old Info Pages*/}
-        <Route path="/password-info" element={<PasswordInfo />} />
-        <Route path="/phishing-info" element={<PhishingInfo />} />
-        <Route path="/social-media-info" element={<SocialMediaInfo />} />
-
         {/*New Info Pages*/}
         <Route path="/passwordchecker" element={<PasswordSecurityPage />} />
         <Route path="/phishing" element={<PhishingTrainingPage />} />
         <Route path="/social" element={<SocialMediaAnalysisPage />} />
         
         {/*Old Tool Pages */}
-        <Route path="/phishing-trainer" element={<PhishingTrainer />} />
         <Route path="/social-media-analyzer" element={<UnifiedSocialMediaAnalyzer />} />
-        <Route path="/password-generator" element={<PasswordGenerator />} />
-        <Route path="/password-checker" element={<PasswordChecker />} />
 
         {/*New Tool Pages*/}
         <Route path="/phishing-trainer-tool" element={<PhishingTrainerTool />} />
